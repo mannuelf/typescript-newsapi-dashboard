3fcdc91d15cdcb373d866d1d9769f352985a7a2f
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-07-11 08:10:26 +0200

    build: rollback postcss to 7

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,3 @@
 export const API_KEY: string = process.env.REACT_APP_API_KEY!;
-export const COUNTRY: string = 'no';
+export const COUNTRY: string = 'us';
 export const URL: string = `http://newsapi.org/v2/top-headlines?country=${COUNTRY}&apiKey=${API_KEY}`;
