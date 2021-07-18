66909574ecb0616da2bf0a41fcc9e1f56a0b0f79
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-07-18 09:06:07 +0200

    build: create header component

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,9 @@
+const Header = (): JSX.Element => {
+  return (
+    <header className="sticky top-0 z-40 lg:z-50 w-full max-w-8xl mx-auto bg-white flex-none flex">
+      <h1 className="text-3xl">Welcome to React news</h1>
+    </header>
+  );
+};
+
+export default Header;
