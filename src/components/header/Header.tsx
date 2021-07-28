30c82480f704f364d5662dc387a92f0ca3370362
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-07-28 12:45:29 +0200

    styling: ui styling some drop shadows and title

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,7 @@
 const Header = (): JSX.Element => {
   return (
-    <header className="sticky top-0 z-40 lg:z-50 w-full max-w-8xl mx-auto bg-white flex-none flex">
-      <h1 className="text-3xl">Welcome to React news</h1>
+    <header className="sticky top-0 z-40 py-1 px-4 lg:z-50 w-full max-w-8xl mx-auto bg-white flex-none flex shadow-md">
+      <h1 className="text-2xl">News</h1>
     </header>
   );
 };
