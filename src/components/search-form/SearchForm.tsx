0aa0ef194775637f6944845d0d5ac42d5621efb6
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-07-28 12:43:57 +0200

    build: integrate useDebounce hook with search form

--- src/components/search-form/SearchForm.tsx
+++ src/components/search-form/SearchForm.tsx
@@ -1,45 +1,52 @@
 import { useDispatch } from 'react-redux';
-import { useEffect, useState } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 import { setSearch } from 'store/reducers/SearchReducer';
 import { fetchArticles } from 'store/reducers/ArticlesReducer';
 import { useAppDispatch, useAppSelector } from 'hooks';
+import useDebounce from 'hooks/useDebounce';
 
 const SearchForm = (): JSX.Element => {
-  const [searchTerm, setSearchTerm] = useState('');
+  const [value, setValue] = useState<string>('');
+  const debouncedValue = useDebounce<string>(value, 1000);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    dispatch(setSearch(searchTerm));
-    dispatch(fetchArticles());
-  }, [searchTerm, dispatch]);
+    if (debouncedValue) {
+      dispatch(setSearch(debouncedValue));
+      dispatch(fetchArticles(debouncedValue));
+    }
+  }, [debouncedValue, dispatch]);
 
-  function handleChange(e: any) {
-    setSearchTerm(e.target.value);
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    e.preventDefault();
+    setValue(e.target.value);
   }
 
-  function handleSubmit(e: any) {
+  function handleSubmit(e: ChangeEvent<HTMLFormElement>) {
     e.preventDefault();
   }
 
   return (
-    <form id="searchForm" onSubmit={handleSubmit}>
-      <label
-        htmlFor="search"
-        aria-labelledby="search"
-        className="block text-sm font-medium text-gray-700"
-      >
-        Search news
-      </label>
-      <input
-        type="text"
-        name="search"
-        id="search"
-        value={searchTerm}
-        onChange={handleChange}
-        onFocus={(e: any) => (e.target.value = '')}
-        className="mt-1 relative rounded-md shadow-sm p-2 w-full"
-      />
-    </form>
+    <section className="py-4 px-4">
+      <form id="searchForm" onSubmit={handleSubmit}>
+        <label
+          htmlFor="search"
+          aria-labelledby="search"
+          className="block text-sm font-medium text-gray-700"
+        >
+          Search news, {value}
+        </label>
+        <input
+          type="text"
+          name="search"
+          id="search"
+          value={value}
+          onChange={handleChange}
+          onFocus={(e: any) => (e.target.value = '')}
+          className="mt-1 relative rounded-md shadow-sm p-2 w-full"
+        />
+      </form>
+    </section>
   );
 };
 
